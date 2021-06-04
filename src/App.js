ed57560f4c0527bf181dee43c64c4274beec6165
ssonko94 <ssonkojimmy@gmail.com>
2021-06-04 10:07:02 +0300

    refactor the display content state into a variable to make the jsx code clean

--- src/App.js
+++ src/App.js
@@ -34,18 +34,26 @@ function App() {
     }
     setIsLoading(false);
   }
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
